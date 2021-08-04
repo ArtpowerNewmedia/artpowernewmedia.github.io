a0b92506fd51fb66fab71459e1ee84fd0e4f6642
nikki <moniqshiin@gmail.com>
2021-08-05 07:14:15 +0800

    上傳 pc / unity 版本

---
+++ pili/pc/__settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1204734.json";
+CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 52567623, 52567637, 52639652, 52627396, 52639587, 52627433, 52639660, 52639533, 52627271, 52627304, 52639813, 52640026, 52640459, 52640463, 52640492, 52652982, 52653125, 52656203, 52657470, 52657591, 52657614, 52742410, 52795003, 52906545, 52906608, 52912166 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/52807941/1/basis.wasm.js', 'wasmUrl' : 'files/assets/52807942/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/52807940/1/basis.js', 'preload' : false},
+];
