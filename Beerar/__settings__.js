ac09cc2940e2a922174e5796e11be6d02bfb77ed
nikki <moniqshiin@gmail.com>
2022-07-02 05:56:52 +0800

    上傳酒廠AR

---
+++ Beerar/__settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1459765.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 91185322, 91185361, 91185365, 91185472, 91185439, 91185405, 91185398, 91185328, 91185355, 91185411, 91185435, 91185336, 91185433, 91185359, 91185324, 91185437, 91202163, 91448200, 91471237, 91477512, 91478245, 91478246, 91478307, 91700394, 91702603 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/91185343/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/91185452/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/91185379/1/ammo.js', 'preload' : true},
+];
