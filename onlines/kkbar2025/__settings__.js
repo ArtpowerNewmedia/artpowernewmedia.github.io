113e888be375dbfea030b9ec9400330519f48afe
UsagiHime5566 <51130360+UsagiHime5566@users.noreply.github.com>
2025-07-04 03:00:27 +0800

    看git 會不會比較快

---
+++ onlines/kkbar2025/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2265393.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 236131529, 238616018, 238613271, 236085659, 236953971, 237021466, 237023891, 238613270, 238873466 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/236076327/1/basis.wasm.js', 'wasmUrl' : 'files/assets/236076325/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/236076326/1/basis.js', 'preload' : false},
+];
